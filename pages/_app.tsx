c2272d4590034c192395b03cb6709b54ffe38a9d
woohm402 <woohm404@gmail.com>
2021-09-12 20:23:08 +0900

    react-query 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
 import '../styles/globals.css';
 import React from 'react';
 
+import axios from 'axios';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: async (queryKey) => {
+        return axios.get(queryKey.queryKey[0] as string);
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 export default MyApp;
